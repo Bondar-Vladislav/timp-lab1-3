249c072bbe04bae8df00ff0f6ee0e95f353a59c6
Bondar Vladislav <72622783+Bondar-Vladislav@users.noreply.github.com>
2021-04-26 18:41:23 +0500

    Add files via upload

---
+++ TCP/Client/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+
+    class Program
+    {
+        static async Task Main (string [] args)
+        {
+            Console.Write ("Запуск Client:");
+            Console.WriteLine("Введите номер порта");
+            int portNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите IP");
+            var address =Console.ReadLine();
+            TcpClient client = new TcpClient ();
+            try
+            {
+                client.Connect (address, portNumber);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine ("Не удалось подключиться к серверу.");
+                return;
+            }
+            var stream = client.GetStream ();
+            client.ReceiveBufferSize = 1024 * 1024;
+            var buffer = new byte [client.ReceiveBufferSize];
+            var lengthBuffer = new byte [8];
+            try
+            {
+                while (client.Connected)
+                {
+                    await stream.ReadAsync (lengthBuffer, 0, lengthBuffer.Length);
+                    var size = BitConverter.ToInt64 (lengthBuffer, 0);
+                        stream.WriteByte (1);
+                        await stream.ReadAsync (lengthBuffer, 0, lengthBuffer.Length);
+                        var c = await stream.ReadAsync (buffer, 0, (int) BitConverter.ToInt64 (lengthBuffer, 0));
+                        var name = Encoding.Unicode.GetString (buffer, 0, c);
+                        Console.Write ("Введите путь для сохранения файла: ");
+                        string pathFile = Console.ReadLine ();
+                        DirectoryInfo createFolder = new DirectoryInfo (pathFile);
+                        createFolder.Create ();
+                        var fs = File.OpenWrite ($"{pathFile}" + @"\" + name);
+                        long readed = 0;
+                        while (readed < size)
+                        {
+                            var count = await stream.ReadAsync (buffer, 0, buffer.Length);
+                            readed += count;
+                            await fs.WriteAsync (buffer, 0, count);
+                        }
+                        fs.Close ();
+                        Console.Write ($"Файл {name} сохранен.");
+                }
+            }
+            catch (SocketException)
+            {
+                Console.Write ("Сервер прервал соединие");
+            }
+        }
+    }
+}
---
+++ TCP/Server/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+
+        static async Task Main (string [] args)
+        {
+            Console.Write ("1. Запуск Server:");
+            Console.WriteLine("Введите номер порта");
+            int portNumber = Convert.ToInt32(Console.ReadLine());
+            Server server = new Server (portNumber);
+            var _ = Task.Factory.StartNew (server.Start, TaskCreationOptions.LongRunning);
+            server.ClientConnected += () =>
+            {
+                Console.Clear ();
+                for (int i = 0; i < server.Clients.Count; i++)
+                {
+                    var client = server.Clients [i];
+                }
+            };
+            Console.Write ("Ожидание подключения клиента");
+            Process myProcess = new Process ();
+            myProcess.StartInfo.FileName = @"C:\Users\3000v\OneDrive\Рабочий стол\TCP - без winapi\Client\bin\Debug\Client.exe";
+            myProcess.Start ();
+            await server.WaitClientConnection ();
+            Console.WriteLine("Введите номер клиента");
+            int clientIndex = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Укажите путь к файлу");
+            string pathToFile = Console.ReadLine();
+            await server.Clients [clientIndex].SendFile (pathToFile);
+            myProcess.Kill ();
+        }
+    }
+}
