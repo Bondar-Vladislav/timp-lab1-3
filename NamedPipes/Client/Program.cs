d46a672ff3b9d9e9fc87d12d582082b9df765f30
Bondar Vladislav <72622783+Bondar-Vladislav@users.noreply.github.com>
2021-04-26 18:47:22 +0500

    Add files via upload

---
+++ NamedPipes/Client/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Text;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя сервера");
+            NamedPipeClientStream clientStream = new NamedPipeClientStream(Console.ReadLine()); 
+            clientStream.Connect();
+            Console.WriteLine("Подключение успешно");
+            byte[] fileBuffer = new byte[1024 * 1024];
+            try
+            {
+                while (true)
+                {
+                int length = clientStream.ReadByte();
+                byte[] buffer = new byte[length * 2];
+                clientStream.Read(buffer, 0, buffer.Length);
+                string name = Encoding.Unicode.GetString(buffer);
+                clientStream.Read(buffer, 0, buffer.Length);
+                long size = BitConverter.ToInt64(buffer, 0);
+                Console.WriteLine("Начинается загрузка файла");
+                clientStream.WriteByte(1);
+                long readed = 0;
+                var fs = File.OpenWrite(name);
+                 while (readed < size)
+                 {
+                    int count = clientStream.Read(fileBuffer, 0, fileBuffer.Length);
+                    fs.Write(fileBuffer, 0, count);
+                    readed += count;
+                 }
+                fs.Close();
+                Console.WriteLine("Файл загружен");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ NamedPipes/Server/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Text;
+using System.Diagnostics;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя сервера:");
+            NamedPipeServerStream server = new NamedPipeServerStream(Console.ReadLine(), PipeDirection.InOut);
+            Process myProcess = new Process();
+            myProcess.StartInfo.FileName = @"C:\Users\3000v\OneDrive\Рабочий стол\NamedPipes\Client\bin\Debug\Client.exe";
+            myProcess.Start();
+            server.WaitForConnection();
+            Console.WriteLine("Клиент подключен");
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("Введите путь к файлу:");
+                    var file = Console.ReadLine();
+                    if (!File.Exists(file))
+                        continue;
+                    string fileName = Path.GetFileName(file);
+                    server.WriteByte((byte)fileName.Length);
+                    var fileNameBytes = Encoding.Unicode.GetBytes(fileName);
+                    server.Write(fileNameBytes, 0, fileNameBytes.Length);
+                    using (var stream = File.OpenRead(file))
+                    {
+                        var lengthBytes = BitConverter.GetBytes(stream.Length);
+                        server.Write(lengthBytes, 0, lengthBytes.Length);
+                        if (server.ReadByte() == 1)
+                        {
+                            Console.WriteLine("Отправка файла");
+                            while (stream.Position < stream.Length)
+                            {
+                                stream.CopyTo(server, 1024 * 1024);
+                            }
+                            Console.WriteLine("Файл отправлен");
+                        }
+                }   }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
